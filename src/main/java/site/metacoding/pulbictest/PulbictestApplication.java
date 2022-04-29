335539c62264e8e193bf9872c7fe0157362203d9
milo <mybule8@naver.com>
2022-04-29 18:34:57 +0900

    과제완료

---
+++ src/main/java/site/metacoding/pulbictest/PulbictestApplication.java
@@ -0,0 +1,13 @@
+package site.metacoding.pulbictest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PulbictestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PulbictestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/site/metacoding/pulbictest/PulbictestApplicationTests.java
@@ -0,0 +1,13 @@
+package site.metacoding.pulbictest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PulbictestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
